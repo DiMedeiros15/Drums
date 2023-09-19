cab2ba929fee98186e0c26d6fe8be04363cd3fed
Diego Medeiros <69081730+DiMedeiros15@users.noreply.github.com>
2023-09-18 21:44:23 -0300

    add first commit
    project drums

---
+++ script.js
@@ -0,0 +1,37 @@
+document.body.addEventListener('keyup', (event)=>{
+    playSound(event.code.toLowerCase())
+})
+
+document.querySelector(".composer button").addEventListener('click', () =>{
+const song = document.querySelector("#input").value;
+
+    if (song !== ''){
+        let songArray = song.split('')
+        playComposition(songArray)
+    }
+})
+
+function playSound(sound){
+    let audio = document.getElementById(`s_${sound}`)
+    let active = document.querySelector(`div[data-key="${sound}"]`)
+
+    if (audio) {     
+        audio.play();   
+    }
+
+    if (active) {
+        active.classList.add('active')
+
+        setTimeout(()=>{
+            active.classList.remove('active')}, 200);        
+    }
+}
+
+function playComposition(songArray){
+    let wait = 0;
+
+    songArray.forEach(song => {
+        setTimeout(()=>{ playSound(`key${song}`)}, wait)       
+        wait += 250;
+    });
+}
